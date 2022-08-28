b8a7ee7de9eb93c60ae53ce827ed01d6e64ce198
Hayk <112321263+Hayk111188@users.noreply.github.com>
2022-08-28 16:25:02 +0400

    Delete changing txt.py

--- changing txt.py
+++
@@ -1,6 +0,0 @@
-
-import pandas as pd
-import s3fs
-path = 'C://Python projects//MySQL genetic database//cnv_processed'
-df = pd.read_csv(path + '.txt' , delim_whitespace=True)
-df.to_excel(path + '.xlsx' , index = False)
